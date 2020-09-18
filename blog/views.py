a5df8e2d95f0a4880d41df8db22dd18747c3346d
Tanvi <tanvisutar888@gmail.com>
2020-09-18 21:35:29 +0530

    Added model class Post to blog/models.py file. Changed blog/admin.py file to reflect the newly created Post model in the admin site. Modified blog/views.py file to fetch the data from the database and render it in the webpages. Changed blog/template/home.html to change the format of the date being displayed in the blogs.

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 def home(request):
     context = {
-        'posts':[
-            {
-                'author': 'Jane Austin',
-                'title':'Pride and prejudice',
-                'date':'August 29,2018',
-                'content':'Nobody can talk me out my  opinion.'
-            },
-            {
-                'author': 'Shakespear',
-                'title':'Othello',
-                'date':'December 07,2014',
-                'content':'Whats in  name. A rose would smell the same even if named otherwise.'
-            }
-        ],
-        'title':'Blog Home'
+        'title':'Blog Home',
+        'posts' : Post.objects.all()
     }
     return render(request,'blog/home.html',context)
