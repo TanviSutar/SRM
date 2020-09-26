b09cf2c5188680de7e4477ea4ebe5c2bdf82464f
Tanvi <tanvisutar888@gmail.com>
2020-09-26 15:06:18 +0530

    Added functionality to create, delete and update posts. Created another page that gives detailed view of the blog posts.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='blog_home'),
+    path('',PostListView.as_view(),name='blog_home'),
+    path('post/<int:pk>/',PostDetailView.as_view(),name='post_detail'),
+    path('post/new/',PostCreateView.as_view(),
+    name='post_create'),
+    path('post/<int:pk>/update',PostUpdateView.as_view(),name='post_update'),
+    path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post_delete'),
 ]
