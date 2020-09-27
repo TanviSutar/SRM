907206b822c294bd00d79234f6193ee4d2043817
Tanvi <tanvisutar888@gmail.com>
2020-09-27 22:51:22 +0530

    Removed the harcoded confidential information from the settings file and fetched it using environment varibles.

--- SRM/settings.py
+++ SRM/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e#ct!^z8$8o99i%k&0%lr_boi^!=@42(j)9t-t@#(wxhphkuc6'
+SECRET_KEY = os.environ.get('SECRET_KEY_BLOG')
+#'e#ct!^z8$8o99i%k&0%lr_boi^!=@42(j)9t-t@#(wxhphkuc6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -141,5 +142,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True 
-EMAIL_HOST_USER = 'tanvisutar888@gmail.com'
-EMAIL_HOST_PASSWORD = 'dzpugqjbtdyuseqr'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
