1ab7c4f5eaafb9c5782229d43f3ff22699c50f6c
Tanvi <tanvisutar888@gmail.com>
2020-09-28 11:17:32 +0530

    Changes to settings and url file for some security purposes.

--- SRM/settings.py
+++ SRM/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY_BLOG')
-#'e#ct!^z8$8o99i%k&0%lr_boi^!=@42(j)9t-t@#(wxhphkuc6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
